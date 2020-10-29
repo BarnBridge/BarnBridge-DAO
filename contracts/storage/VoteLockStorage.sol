b0dc02e54c178d394312cdea0b2a4a2731e0d588
Casian Lacatusu <casian.lacatusu@gmail.com>
2020-10-29 10:15:39 +0200

    Rename constant name for votelock storage position

--- contracts/storage/VoteLockStorage.sol
+++ contracts/storage/VoteLockStorage.sol
@@ -3,16 +3,14 @@ pragma solidity ^0.7.1;
 pragma experimental ABIEncoderV2;
 
 contract VoteLockStorage {
-    bytes32 constant DIAMOND_STORAGE_POSITION = keccak256("com.barnbridge.dao.votelock");
+    bytes32 constant VOTELOCK_STORAGE_POSITION = keccak256("com.barnbridge.dao.votelock");
 
     struct VoteLockStorage {
         mapping(address => uint256) balances;
-
-        mapping(bytes4 => bool) supportedInterfaces;
     }
 
     function voteLockStorage() internal pure returns(VoteLockStorage storage ds) {
-        bytes32 position = DIAMOND_STORAGE_POSITION;
+        bytes32 position = VOTELOCK_STORAGE_POSITION;
         assembly { ds.slot := position }
     }
 }
